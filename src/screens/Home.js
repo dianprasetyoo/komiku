87f0884ac67b47defd549337d4fbcbd95c2c49f0
dianprasetyoo <dipra94@gmail.com>
2019-10-21 11:56:02 +0700

    first commit

---
+++ src/screens/Home.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { SafeAreaView, Text, FlatList } from 'react-native'
+
+export default class Home extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      data: [
+        { id: 1, name: 'Lucinta Luna' },
+        { id: 2, name: 'Lucinta Luci' },
+        { id: 3, name: 'Kuwe Luna' },
+        { id: 4, name: 'Lucinta Kamu' },
+        { id: 5, name: 'Kamu Cinta Luna' },
+        { id: 6, name: 'Sakarepmu Luna' },
+      ]
+    }
+  }
+
+
+  render() {
+
+    return (
+      <SafeAreaView>
+
+        <FlatList
+          data={this.state.data}
+          renderItem={({item}) => <Text> {item.name} </Text>}
+          keyExtractor={item => item.id} />
+
+      </SafeAreaView>
+    )
+  }
+}
